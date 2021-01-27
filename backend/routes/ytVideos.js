da017a8215348b837cc64f0f7bf66647cf8b1587
Guillaume Franco <gfranco@z3r6p5.42lyon.fr>
2021-01-27 17:07:16 +0100

    back - edit route fixed

--- backend/routes/ytVideos.js
+++ backend/routes/ytVideos.js
@@ -7,8 +7,11 @@ const pool = require('../db');
 router.post('/', async(req, res) => {
     try {
       const {title, url, category, ep_nb} = req.body;
+      const searchVideo = await pool.query("SELECT * FROM ytvideos WHERE url = $1", [url]);
+      if (searchVideo.rows.length !== 0)
+        return res.status(401).json("Cette URL existe déjà!");
       const newVideo = pool.query("INSERT INTO ytvideos (title, url, category, ep_nb) VALUES ($1, $2, $3, $4)", [title, url, category, ep_nb]);
-      res.json(newVideo.rows);
+      res.json("Ajout réussi !");
     } catch (error) {
       console.error(error.message);
     }
@@ -28,13 +31,25 @@ router.get('/', async (req, res) => {
 // show one video
 router.get('/:id', async (req, res) => {
     try {
-       const {id} = req.params;
-       const getVideo = pool.query("SELECT * FROM ytvideos WHERE id = $id", [id]);
-       res.json(getVideo.rows); 
+        const {id} = req.params;
+        const getVideo = pool.query("SELECT * FROM ytvideos WHERE id = $id", [id]);
+        res.json(getVideo.rows); 
     } catch (error) {
         console.error(error.message);
         res.status(500).send("Server Error");
     }
+})
+
+// show all videos from one category
+router.get('/:category', async (req, res) => {
+  try {
+      const {category} = req.params;
+      const getVideo = pool.query("SELECT * FROM ytvideos WHERE category = $1", [category]);
+      res.json(getVideo.rows); 
+  } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+  }
 })
 
 // update
@@ -44,15 +59,23 @@ router.put('/:id', async(req, res) => {
       const {id} = req.params;
       const {title, url, category, ep_nb} = req.body;
       let updateVideo;
+      const searchVideo = await pool.query("SELECT * FROM ytvideos WHERE url = $1", [url]);
+
+      // 2. check if user exists (if not, throw error)
+      console.log(searchVideo.rows[0].id)
+      console.log(id)
+      if (searchVideo.rows.length !== 0 && searchVideo.rows[0].id !== id)
+        return res.status(401).json("Cette URL existe déjà!");
+
       if (title !== "" && title !== null)
-        updateVideo = pool.query("UPDATE users SET title = $1 WHERE id = $2", [title, id]);
+        updateVideo = await pool.query("UPDATE ytvideos SET title = $1 WHERE id = $2", [title, id]);
       if (url !== "" && url !== null)
-        updateVideo = pool.query("UPDATE users SET url = $1 WHERE id = $2", [url, id]);
+        updateVideo = await pool.query("UPDATE ytvideos SET url = $1 WHERE id = $2", [url, id]);
       if (category !== "" && category !== null)
-        updateVideo = pool.query("UPDATE users SET category = $1 WHERE id = $2", [category, id]);
+        updateVideo = await pool.query("UPDATE ytvideos SET category = $1 WHERE id = $2", [category, id]);
       if (ep_nb !== "" && ep_nb !== null)
-        updateVideo = pool.query("UPDATE users SET ep_nb = $1 WHERE id = $2", [ep_nb, id]);
-      res.json(`User with id ${id} has been updated ! The new user is now => Age: ${age} => url: ${url} => category: ${category} => ep_nb: ${ep_nb}`);
+        updateVideo = await pool.query("UPDATE ytvideos SET ep_nb = $1 WHERE id = $2", [ep_nb, id]);
+      res.json("Modification réussi !");
   
     } catch (error) {
       console.error(error.message);
