5bf254ebc1332848eed386b32c81f92aaba56ec8
Guillaume Franco <gfranco@z3r6p5.42lyon.fr>
2021-02-01 16:05:48 +0100

    front - add admin restriction

--- frontend/src/component/dashboard.js
+++ frontend/src/component/dashboard.js
@@ -72,10 +72,8 @@ class Dashboard extends Component {
                     <div>{this.state.pseudo}</div>
                     <div>{this.state.email}</div>
                     <div>{this.state.status}</div>
-                    <a href="/admin">Page Admin !</a>
                     <a href="/profil">Page Profil !</a>
                     <a href="/gallery">Page Gallery !</a>
-                    <a href="/videos">Page Videos !</a>
                 </div>
                 <this.adminPage />
             </div>
--- frontend/src/pages/admin.js
+++ frontend/src/pages/admin.js
@@ -6,15 +6,30 @@ import Header from "../component/header.js"
 import Footer from "../component/footer"
 
 
-const Admin = () =>
-    <div className="admin">
-        <Header />
-        <div className="admin__title">
-            <h1>Admin</h1>
+const Admin = async () => {
+
+    const response = await fetch("http://localhost:9000/profil/", {
+        method: "GET",
+        headers: {token: localStorage.token}
+    })
+    const parseRes = await response.json();
+    const {id, age, pseudo, email, status} = parseRes;
+    if (status !== 'admin') {
+        localStorage.setItem("error", "Vous n'avez pas les droits administrateurs !!");
+        window.location = '/error';
+    }
+    return (
+        <div className="admin">
+            <Header />
+            <div className="admin__title">
+                <h1>Admin</h1>
+            </div>
+            <AdminUserManagement />
+            <AdminYTVideosManagement />
+            <Footer />
         </div>
-        <AdminUserManagement />
-        <AdminYTVideosManagement />
-        <Footer />
-    </div>
+    )
+    
+}
 
 export default Admin;
