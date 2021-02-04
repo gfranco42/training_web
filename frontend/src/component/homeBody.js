db0ec3790571668f1ce216d15a7fbe5e2d7d7864
Guillaume Franco <gfranco@z3r6p5.42lyon.fr>
2021-02-04 10:42:46 +0100

    front - update home page with video and waves

--- frontend/src/component/homeBody.js
+++ frontend/src/component/homeBody.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
 
+// GIFS
+import wave_body from "../img/gifs/wave_body.gif";
+
+// REACT PLAYER
+import ReactPlayer from "react-player";
+
 class HomeBody extends Component {
     constructor(props) {
         super(props)
@@ -11,7 +17,16 @@ class HomeBody extends Component {
     render() {
         return (
             <div className="home-body">
-                <h1 className="home-body__title">Bienvenue sur le site Asylum Heroes !</h1>
+                <div className="home-header">
+                    <h1 className="">Bienvenue sur le site Asylum Heroes !</h1>
+                </div>
+                <div className="home-main">
+                    <img src={wave_body} alt="wave_gif" className="home-main__wave"/>
+                    <ReactPlayer url={"https://asylum-heroes.s3.eu-west-3.amazonaws.com/ending_tw.mp4"}
+                        loop={true} onReady={ () => {} } playing={true} volume={0}
+                        width="100%" height="100%"
+                        className="home-main__video"/>
+                </div>
             </div>
         )
     }
