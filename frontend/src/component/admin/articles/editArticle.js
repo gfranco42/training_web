9ba4d5f676c7fc649e76232fd61bc8c887b0f17f
Guillaume Franco <gfranco@z3r6p5.42lyon.fr>
2021-02-17 14:50:23 +0100

    front - admin article editting done

--- frontend/src/component/admin/articles/editArticle.js
+++ frontend/src/component/admin/articles/editArticle.js
@@ -3,6 +3,13 @@ import { toast } from 'react-toastify';
 
 import Popup from 'reactjs-popup'
 
+// URL GENERATOR
+import S3FileUpload from 'react-s3';
+
+/* FUNCTIONS */
+// import { stringCut } from '../../../utils'
+
+
 
 class EditArticle extends Component {
     constructor(props) {
@@ -13,7 +20,14 @@ class EditArticle extends Component {
             description: props.description,
             text_content: props.text_content,
             img_content: props.img_content,
+            oldImg_content: props.img_content,
             video_content: props.video_content,
+            oldVideo_content: props.video_content,
+            current_video: "",
+            access_key: process.env.REACT_APP_ACCESS_KEY,
+            secret_key: process.env.REACT_APP_SECRET_KEY,
+            bucket_name: process.env.REACT_APP_BUCKET,
+            region: process.env.REACT_APP_REGION,
         }
     }
 
@@ -21,6 +35,87 @@ class EditArticle extends Component {
         e.preventDefault();
         this.setState({[e.target.name]: e.target.value});
     }
+
+    // FILE RELATED
+    updateArticleFile = async (e) => {
+        e.preventDefault();
+        const {access_key, secret_key, bucket_name, region} = this.state;
+        const config = {
+            bucketName: bucket_name,
+            dirName: "",
+            accessKeyId: access_key,
+            secretAccessKey: secret_key,
+            region: region,
+        };
+
+        const data = await S3FileUpload.uploadFile(e.target.files[0], config)
+        const url = data.location;
+        this.setState({image: url});// remove "name" 
+    }
+    //******************************************* */
+
+    // IMG RELATED
+    updateArticleMultipleFiles = async (e) => {
+        e.preventDefault();
+        const {access_key, secret_key, bucket_name, region} = this.state;
+        const config = {
+            bucketName: bucket_name,
+            dirName: "",
+            accessKeyId: access_key,
+            secretAccessKey: secret_key,
+            region: region,
+        };
+
+        const len = e.target.files.length;
+        var tab = [];
+        for (let i = 0; i < len; i++) {
+            const data = await S3FileUpload.uploadFile(e.target.files[i], config)
+            const url = data.location;
+            tab[i] = url;//remove "name"
+        }
+        tab.map((item) => {
+            return this.setState({img_content: [...this.state.img_content, item]})
+        })
+    }
+
+    deleteImg = (e, idx) => {
+        e.preventDefault();
+        const data = this.state.img_content;
+        this.setState({img_content: data.filter( (name, index) => index !== idx) });
+    }
+
+    resetImgs = (e) => {
+        e.preventDefault();
+        this.setState({img_content: this.state.oldImg_content})
+    }
+    //******************************************* */
+
+    // LINK RELATED
+    addVideoLink = async (e) => {
+        e.preventDefault();
+        const {current_video} = this.state;
+        current_video ? this.setState({video_content: [...this.state.video_content, current_video]})
+        : toast.error("Aucune URL renseignée !", {
+            className: "toast",
+            position: "top-center",
+            hideProgressBar: true,
+            closeButton: false,
+        })
+    }
+
+    deleteLink = (e, idx) => {
+        e.preventDefault();
+        const data = this.state.video_content;
+        this.setState({video_content: data.filter( (name, index) => index !== idx) });
+    }
+
+    resetLinks = (e) => {
+        e.preventDefault();
+        this.setState({video_content: this.state.oldVideo_content})
+    }
+    //******************************************* */
+
+
 
     editArticle = async (e, id) => {
         e.preventDefault();
@@ -81,7 +176,7 @@ class EditArticle extends Component {
                                     Miniature 
                                     <input type="file"
                                         accept="image/*"
-                                        onChange={(e) => {this.updateArticleInfo(e)}}
+                                        onChange={(e) => {this.updateArticleFile(e)}}
                                         name="image">
                                     </input>
                                 </label>
@@ -93,7 +188,7 @@ class EditArticle extends Component {
                                     closeOnDocumentClick
                                 >
                                     {close => (
-                                        <div className="edit-popup__showImg">
+                                        <div className="edit-popup__showContent">
                                            <img src={this.state.image} alt="article-img"></img>
                                         </div>
                                         )
@@ -117,28 +212,80 @@ class EditArticle extends Component {
                                     name="text_content">
                                 </textarea>
                             </label>
-                            <label className="edit-popup--file">
-                                Contenu image(s)
-                                <input
-                                    type="file"
-                                    onChange={(e) => {this.updateArticleInfo(e)}}
-                                    name="img_content"
-                                    multiple
-                                    >
-                                </input>
-                            </label>
-                            <label>
-                                Contenu video(s):
-                                <input
-                                    type="text"
-                                    onChange={(e) => {this.updateArticleInfo(e)}}
-                                    name="current_video">
-                                </input>
-                            </label>
-                            {/* <button onClick={this.addLink()} className="edit-popup--linkAdding">
-                                Ajouter un lien
-                            </button> */}
-                            <button className="edit-popup--linkAdding">Ajouter un lien</button>
+                            <div className="edit-popup__file">
+                                <label className="edit-popup__file--input">
+                                    Contenu image(s)
+                                    <input
+                                        type="file"
+                                        onChange={(e) => {this.updateArticleMultipleFiles(e)}}
+                                        name="img_content"
+                                        multiple
+                                        >
+                                    </input>
+                                </label>
+                                <Popup trigger={ open => (
+                                    <div className="edit-popup__file--show">
+                                        {open ? 'Cacher' : 'Afficher'}
+                                    </div>)}
+                                    position="right bottom"
+                                    closeOnDocumentClick
+                                >
+                                    {close => (
+                                        <div className="edit-popup__showContent">
+                                            <button className="edit-popup__showContent--reset" onClick={this.resetImgs}>
+                                                Réinitialiser
+                                            </button>
+                                            <div className="edit-popup__showContent--imgContent">
+                                                {this.state.img_content.length > 0 ? this.state.img_content.map( (item, index) =>
+                                                    <div key={index} className="edit-popup__showContent--block">
+                                                        <img src={item} alt="article-img" className="img_content"></img>
+                                                        <button onClick={(e) => {this.deleteImg(e, index)}}>X</button>
+                                                    </div>
+                                                ) : "auncun contenu"}
+                                            </div>
+                                        </div>
+                                        )
+                                    }
+                                </Popup>
+                            </div>
+                            <div className="edit-popup__file">
+                                <label>
+                                    Contenu video(s):
+                                    <input
+                                        type="text"
+                                        onChange={(e) => {this.updateArticleInfo(e)}}
+                                        name="current_video">
+                                    </input>
+                                </label>
+                                <button onClick={(e) => this.addVideoLink(e)} className="edit-popup--linkAdding">
+                                    Ajouter un lien
+                                </button>
+                                <Popup trigger={ open => (
+                                    <div className="edit-popup__file--show">
+                                        {open ? 'Cacher' : 'Afficher'}
+                                    </div>)}
+                                    position="right bottom"
+                                    closeOnDocumentClick
+                                >
+                                    {close => (
+                                        <div className="edit-popup__showContent">
+                                            <button className="edit-popup__showContent--reset" onClick={this.resetLinks}>
+                                                Réinitialiser
+                                            </button>
+                                            {this.state.video_content.length > 0 ?
+                                                // <div className="edit-popup__showContent--block video-popup">
+                                                    this.state.video_content.map( (item, index) => 
+                                                        <div className="edit-popup__showContent--block video-popup" key={index}>
+                                                            <div className="video-popup--item">{item}</div>
+                                                            <button onClick={(e) => {this.deleteLink(e, index)}}>
+                                                                X
+                                                            </button>
+                                                        </div>
+                                                    ) : "auncun contenu"}
+                                        </div>
+                                    )}
+                                </Popup>
+                            </div>
                             <input
                                 className="edit-popup--submit"
                                 type="submit"
@@ -146,7 +293,8 @@ class EditArticle extends Component {
                                 onClick={(e) => {
                                     this.editArticle(e, this.props.articleId);
                                     close()
-                            }}>
+                                }}
+                            >
                             </input>
                         </form>
                         <button className="edit-popup--closeBtn" onClick={close}>Fermer</button>
